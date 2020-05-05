f80071f747e01a630f1e7a97fb82ddd24aeba541
alexcool <alex@hash.kiev.ua>
2020-05-05 02:42:54 +0200

    added mediator interface

---
+++ ControllerMediator.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOANS_projekt
+{
+
+// Mediates the common tasks
+class ControllerMediator : Mediator
+{
+    public Controller BattleController { get; set; }
+    public Controller MarketplaceController { get; set; }
+
+    public void SwitchMode(Controller.ControllerType type)
+    {
+        switch (type){
+            case Controller.ControllerType.Battlefield : 
+                this.BattleController.Commence(type);
+                break;
+            case Controller.ControllerType.Marketplace : 
+                this.MarketplaceController.Commence(type);
+                break;
+        }
+    }
+}}
--- Mediator.cs
+++ Mediator.cs
@@ -8,20 +8,7 @@ namespace OOANS_projekt
 {
 
 // Mediates the common tasks
-public class Mediator
+public interface Mediator
 {
-    public Controller BattleController { get; set; }
-    public Controller MarketplaceController { get; set; }
-
-    public void SwitchMode(Controller.ControllerType type)
-    {
-        switch (type){
-            case Controller.ControllerType.Battlefield : 
-                this.BattleController.Commence(type);
-                break;
-            case Controller.ControllerType.Marketplace : 
-                this.MarketplaceController.Commence(type);
-                break;
-        }
-    }
+    public void SwitchMode(Controller.ControllerType type);
 }}
